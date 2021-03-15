9ced55056a0cee9a931dffed2c2efaf15d04e737
Sam24Hernandez <sam.herco24@gmail.com>
2021-03-15 09:47:18 -0600

    Estructuración inicial del proyecto Yuppie (Patrón MVC)

---
+++ views/template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html> 
+<!-- [if IE 9]> <html class="ie9"> <![endif] -->
+<!-- [if (gt IE 9)|!(IE)]><!-->
+<html lang="es">
+    <!--<![endif] -->
+    <head>
+        <meta charset="utf-8">
+        <!-- Yuppie Icon -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes, minimum-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="Compra y venta de artículos electrónicos, ropa, calzado, artículos deportivos, cursos digitales, cámaras digitales, artículos para bebé, cupones y muchos artículos más en Yuppie, la plataforma de comercio electrónico en México.">
+        <meta name="keywords" content="Yuppie, Yuppie.com.mx, artículos electrónicos, cursos digitales, compras por Internet, compras en linea, yuppie méxico, ropa, cámaras digitales, bebé, herramientas, belleza, relojes, deportes y entretenimiento">
+        <title>Yuppie | Comercio electrónico, artículos, ropa y más</title>
+    </head>
+    <body>
+        <?php echo "<h1>¡Bienvenidos a Yuppie!</h1>"; ?>
+        <?php echo "<p>Empezando la estructuración del proyecto</p>"; ?>
+    </body>
+</html>
+
